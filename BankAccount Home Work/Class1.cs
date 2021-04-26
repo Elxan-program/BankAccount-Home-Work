b70a9a2b26d6869ac72807b88fad6f394ae66ff1
Elxan <Atay_qn28@itstep.edu.az>
2021-04-26 14:01:23 +0400

    Add project files.

---
+++ BankAccount Home Work/Class1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bank_Project;
+
+namespace HelperNameSpace
+{
+    class Hepler
+    {
+        public static bool CheckPAN(string pan)
+        {
+            if (pan.Length == 16)
+            {
+                return true;
+            }
+            else
+            {
+                throw new Exception("PAN character count is not equal 16");
+            }
+        }
+
+        public static bool CheckPIN(string pin)
+        {
+            if (pin.Length == 4)
+            {
+                return true;
+            }
+            else
+            {
+                throw new Exception("PIN character count is not equal 4");
+            }
+        }
+
+        public static bool CheckCVC(string cvc)
+        {
+            if (cvc.Length == 3)
+            {
+                return true;
+            }
+            else
+            {
+                throw new Exception("CVC character count is not equal 3");
+            }
+        }
+
+        public static bool CheckBalance(int balance)
+        {
+            if (balance != 0)
+            {
+                return true;
+            }
+            else
+            {
+                throw new Exception("In this bank card don't have any money");
+            }
+        }
+
+        public static bool CheckExpireTime(DateTime expireTime)
+        {
+            if (expireTime != DateTime.Now)
+            {
+                return true;
+            }
+            else
+            {
+                throw new Exception("Expire time was finished");
+            }
+        }
+    }
+}
---
+++ BankAccount Home Work/Program.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HelperNameSpace;
+
+namespace Bank_Project
+{
+    class BankCard
+    {
+        public string BankName { get; set; }
+
+        public string UserName { get; set; }
+
+        public string PAN { get; set; }
+
+        public string PIN { get; set; }
+
+        public string CVC { get; set; }
+
+        public DateTime ExpireTime { get; set; }
+
+        public int Balance { get; set; }
+
+        public void ShowBalance()
+        {
+            Console.WriteLine($"Balance : {Balance}");
+        }
+
+    }
+    class Client
+    {
+        BankCard bankCard;
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public int Age { get; set; }
+
+        public int Salary { get; set; }
+
+        public void Show()
+        {
+            Console.WriteLine("==========CLIENT==========");
+            Console.WriteLine();
+            Console.WriteLine($"Name : {Name}");
+            Console.WriteLine($"Surname : {Surname}");
+            Console.WriteLine($"Age : {Age}");
+            Console.WriteLine($"Salary : {Salary}");
+            Console.WriteLine($"ID : {Id}");
+        }
+
+    }
+    class Bank
+    {
+        public int ClientCount { get; set; }
+
+        public Client[] Clients { get; set; }
+        public void addClients(ref Client client)
+        {
+            Client[] temp = new Client[++ClientCount];
+            if (Clients != null)
+            {
+                Clients.CopyTo(temp, 0);
+            }
+            temp[temp.Length - 1] = client;
+            Clients = temp;
+
+
+        }
+        public BankCard bankCard { get; set; } = default;
+        public void ShowCardBalance(BankCard BankCard)
+        {
+            try
+            {
+                if (Hepler.CheckPAN(BankCard.PAN))
+                {
+                    if (Hepler.CheckExpireTime(BankCard.ExpireTime))
+                    {
+                        if (Hepler.CheckBalance(BankCard.Balance))
+                        {
+                            bankCard.ShowBalance();
+                        }
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public void ShowClients()
+        {
+            foreach (var item in Clients)
+            {
+                item.Show();
+            }
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            BankCard bankCard1 = new BankCard
+            {
+                BankName = "Bir Bank",
+                UserName = "Obama Obamali",
+                PIN = "1234",
+                PAN = "321245",
+                CVC = "343",
+                ExpireTime = new DateTime(2023,12,10),
+                Balance = 12340
+            };
+            
+            Client c1 = new Client
+            {
+                Name = "Obama",
+                Surname = "Obamali",
+                Age = 45,
+                Id = 1,
+                Salary = 1200
+            };
+            Client c2 = new Client
+            {
+                Name = "Nizami",
+                Surname = "Nizamili",
+                Age = 35,
+                Id = 2,
+                Salary = 2100
+            };
+            Client c3 = new Client
+            {
+                Name = "John",
+                Surname = "Johnoglu",
+                Age = 25,
+                Id = 12,
+                Salary = 4500
+            };
+
+            Bank bank = new Bank { };
+            bank.addClients(ref c1);
+            bank.addClients(ref c2);
+            bank.addClients(ref c3);
+
+            bank.ShowClients();
+
+            bank.ShowCardBalance(bankCard1);
+            
+        }
+    }
+}
+
